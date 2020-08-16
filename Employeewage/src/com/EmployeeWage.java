d41717450829adfcfdeaa73e44d52b3c81590cd0
pradip0016 <patilpradip980@gmail.com>
2020-08-16 11:10:22 +0530

    uc2

--- Employeewage/src/com/EmployeeWage.java
+++ Employeewage/src/com/EmployeeWage.java
@@ -3,12 +3,17 @@ package com;
 import java.util.Random;
 
 public class EmployeeWage {
-
+	int empWageperHer = 20;
+	int full_time = 8;
+	int wage=0;
 	public void dailywage(int ran_int) {
 		System.out.println("value " + ran_int);
 
 		if (ran_int == 1) {
 			System.out.println("Employee is present");
+			System.out.println("Employee is present");
+			wage = empWageperHer + full_time;
+			System.out.println("Employee daily wage is if present: " + wage);
 		} else if (ran_int == 2) {
 			System.out.println("Employee is present part time");
 		} else {
