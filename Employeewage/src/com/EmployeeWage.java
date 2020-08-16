2278a57fe6b5953d2917e954bdd36964b34062e4
pradip0016 <patilpradip980@gmail.com>
2020-08-16 10:18:20 +0530

    branch one

--- Employeewage/src/com/EmployeeWage.java
+++ Employeewage/src/com/EmployeeWage.java
@@ -1,9 +1,29 @@
 package com;
 
+import java.util.Random;
+
 public class EmployeeWage {
 
-	public static void main(String[] args) 
-	{
-	System.out.println("Welcome to Employee wage computation program");	
+	public void dailywage(int ran_int) {
+		System.out.println("value " + ran_int);
+
+		if (ran_int == 1) {
+			System.out.println("Employee is present");
+		} else if (ran_int == 2) {
+			System.out.println("Employee is present part time");
+		} else {
+			System.out.println("Employee is absent");
+
+		}
 	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee wage computation program");
+		EmployeeWage e = new EmployeeWage();
+		Random ran = new Random();
+		int ran_int1 = ran.nextInt(3);
+		e.dailywage(ran_int1);
+
+	}
+
 }
